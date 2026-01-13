9936db1e57d6236065c03d930403a83b16cbf240
centraldepagamentos04-create <centraldepagamentos04@gmail.com>
2026-01-12 21:15:47 -0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,7 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script>
+const supabaseUrl = "https://vlsuusngsmvyknmtbcml.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsc3V1c25nc212eWtubXRiY21sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyNTUzOTQsImV4cCI6MjA4MzgzMTM5NH0.MZDo5SRHwrbxR0ewHUcJiPjYaOcAtqsWnAX-oWoFH6E";
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+</script>
